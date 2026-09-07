be6a2d443888d2f2e160c48b43080056372a356b
awesomeer <47190013+awesomeer@users.noreply.github.com>
2026-09-06 23:20:15 -0500

    STM32L4 base FreeRTOS template

--- init_submodules.sh
+++ init_submodules.sh
@@ -2,3 +2,7 @@ git submodule update --init
 
 cd submodules/STM32CubeL4
 git submodule update --init Drivers/CMSIS/Device/ST/STM32L4xx
+
+cd ../..
+cd submodules/FreeRTOS
+git submodule update --init FreeRTOS/Source
